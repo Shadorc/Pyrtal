7ee1ac772ec780f0891d6b153bfb86df4a93a200
Shadorc <bi-crosseur@hotmail.fr>
2015-04-15 20:31:37 +0200

    Fix Rect.intersects (including changes is better)

--- rectangle.py
+++ rectangle.py
@@ -5,9 +5,29 @@ class Rect():
         self.y = y
         self.width = width
         self.height = height
+        self.centerX = self.x + self.width/2
+        self.centerY = self.y + self.height/2
 
     def intersects(self, rect):
-        if(self.x+self.width < rect.x or rect.x+rect.width < self.x or self.y+self.height < rect.y or rect.y+rect.height < self.y):
-            return False
-        else:
+        if(abs(self.centerX - rect.centerX) < (self.width + rect.width )/2 and abs(self.centerY - rect.centerY) < (self.height+rect.height)/2):
             return True
+        else:
+            return False
+
+    """          width/2
+              <--------->
+    +-------------------+
+    |                   |
+    |       (x, y)      |
+    |         +         |
+    |              +----|---------------+
+    |              |    |               |     |x-x1| < (width+width1)/2 and |y-y1| < (height+height1)/2
+    +-------------------+    (x1, y1)   |
+                   |         +          |
+                   |                    |
+                   |                    |
+                   +--------------------+
+                   <--------->                    
+                    width1/2
+
+    """
